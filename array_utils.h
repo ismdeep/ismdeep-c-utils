6f14571c5209d2fb781ebdeb3a32512bd536b8cf
ismdeep <ismdeep@live.com>
2019-12-25 22:51:11 +0800

    Add array_utils

---
+++ array_utils.h
@@ -0,0 +1,21 @@
+//
+// Created by ismdeep on 2019/12/25.
+//
+
+#ifndef ISMDEEP_C_UTILS_ARRAY_UTILS_H
+#define ISMDEEP_C_UTILS_ARRAY_UTILS_H
+
+void *create_array(size_t size, size_t sizeof_item) {
+    void *arr = malloc(sizeof_item * size);
+    return arr;
+}
+
+void **create_matrix(size_t rows, size_t cols, size_t sizeof_item) {
+    void **arr = (void **) malloc(sizeof(size_t) * rows);
+    for (size_t row_id = 0; row_id < rows; ++row_id) {
+        arr[row_id] = malloc(sizeof_item * cols);
+    }
+    return arr;
+}
+
+#endif //ISMDEEP_C_UTILS_ARRAY_UTILS_H

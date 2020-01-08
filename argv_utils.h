3eed2062990ec187c5332f0890a67c55160d0c12
ismdeep <ismdeep@live.com>
2020-01-08 11:01:30 +0800

    Add argv_utils.h

---
+++ argv_utils.h
@@ -0,0 +1,29 @@
+//
+// Created by ismdeep on 2020/1/5.
+//
+
+#ifndef ISMDEEP_C_UTILS_ARGV_UTILS_H
+#define ISMDEEP_C_UTILS_ARGV_UTILS_H
+
+#include <stdbool.h>
+
+char * get_argv(int argc, char * argv[], const char * search_key) {
+    for (size_t i = 0; i < argc - 1; ++i) {
+        if (strcmp(search_key, argv[i]) == 0) {
+            return argv[i+1];
+        }
+    }
+    return "";
+}
+
+
+bool argv_exist_switch(int argc, char* argv[], const char * search_key) {
+    for (size_t i = 0; i < argc; ++i) {
+        if (strcmp(argv[i], search_key) == 0) {
+            return true;
+        }
+    }
+    return false;
+}
+
+#endif //ISMDEEP_C_UTILS_ARGV_UTILS_H

08144c324b71f2a1d2dbfb83ede7c91fc6554c85
L. Jiang <l.jiang.1024@gmail.com>
2020-12-30 12:05:35 +0800

    update gf2_matrix.h

--- include/ismdeep-c-utils/gf2_matrix.h
+++ include/ismdeep-c-utils/gf2_matrix.h
@@ -29,6 +29,48 @@ struct GF2_Matrix *gf2_matrix_create(size_t _row_, size_t _col_) {
     return matrix;
 }
 
+struct GF2_Matrix *gf2_matrix_eye(size_t size) {
+    struct GF2_Matrix *matrix = gf2_matrix_create(size, size);
+    for (size_t i = 0; i < size; i++) {
+        matrix->data[i][i] = 1;
+    }
+    return matrix;
+}
+
+struct GF2_Matrix *gf2_matrix_concat(struct GF2_Matrix *left, struct GF2_Matrix *right) {
+    if (left->row != right->row) {
+        fprintf(stderr, "ERROR: Size error on gf2_matrix_concat()\n");
+        return NULL;
+    }
+
+    struct GF2_Matrix *matrix = gf2_matrix_create(left->row, left->col + right->col);
+
+    for (size_t i = 0; i < left->row; i++) {
+        for (size_t j = 0; j < left->col; j++) {
+            matrix->data[i][j] = left->data[i][j];
+        }
+        for (size_t j = 0; j < right->col; j++) {
+            matrix->data[i][j + left->col] = right->data[i][j];
+        }
+    }
+
+    return matrix;
+}
+
+void gf2_matrix_transport(struct GF2_Matrix *m1, struct GF2_Matrix *m2) {
+    for (size_t i = 0; i < m1->row; ++i) {
+        for (size_t j = 0; j < m1->col; ++j) {
+            m2->data[j][i] = m1->data[i][j];
+        }
+    }
+}
+
+struct GF2_Matrix *gf2_matrix_transport_func(struct GF2_Matrix *m1) {
+    struct GF2_Matrix *matrix = gf2_matrix_create(m1->col, m1->row);
+    gf2_matrix_transport(m1, matrix);
+    return matrix;
+}
+
 void gf2_matrix_mul(
         const struct GF2_Matrix *matrix_left, const struct GF2_Matrix *matrix_right,
         struct GF2_Matrix *matrix
@@ -57,6 +99,30 @@ struct GF2_Matrix *gf2_matrix_mul_func(const struct GF2_Matrix *matrix_left, con
     return matrix;
 }
 
+void gf2_matrix_add(const struct GF2_Matrix *matrix_left, const struct GF2_Matrix *matrix_right, struct GF2_Matrix *matrix) {
+    if (!(
+            matrix_left->row == matrix_right->row
+            && matrix_left->col == matrix_right->col
+            && matrix_left->row == matrix->row
+            && matrix_left->col == matrix->col
+            )) {
+        fprintf(stderr, "ERROR: Error matrix size on gf2_matrix_add()\n");
+        return;
+    }
+
+    for (size_t i = 0; i < matrix->row; ++i) {
+        for (size_t j = 0; j < matrix->col; ++j) {
+            matrix->data[i][j] = (matrix_left->data[i][j] + matrix_right->data[i][j]) % 2;
+        }
+    }
+}
+
+struct GF2_Matrix *gf2_matrix_add_func(const struct GF2_Matrix *matrix_left, const struct GF2_Matrix *matrix_right) {
+    struct GF2_Matrix *matrix = gf2_matrix_create(matrix_left->row, matrix_left->col);
+    gf2_matrix_add(matrix_left, matrix_right, matrix);
+    return matrix;
+}
+
 void gf2_matrix_copy(const struct GF2_Matrix *matrix_from, struct GF2_Matrix *matrix_to) {
     if (matrix_from->row != matrix_to->row || matrix_from->col != matrix_to->col) {
         fprintf(stderr, "ERROR: gf2_matrix_copy() with wrong size.");
@@ -75,7 +141,7 @@ struct GF2_Matrix *gf2_matrix_copy_func(const struct GF2_Matrix *matrix) {
     return matrix_copied;
 }
 
-char *gf2_matrix_dump(const struct GF2_Matrix *matrix) {
+char *gf2_matrix_dump_core(const struct GF2_Matrix *matrix, char ch0, char ch1) {
     char *ans = (char *) malloc(sizeof(char) * 65535);
     char *tmp = (char *) malloc(sizeof(char) * 65535);
     memset(ans, 0, sizeof(char) * 65535);
@@ -83,13 +149,17 @@ char *gf2_matrix_dump(const struct GF2_Matrix *matrix) {
 
     for (size_t i = 0; i < matrix->row; ++i) {
         for (size_t j = 0; j < matrix->col; ++j) {
-            sprintf(tmp, "%d ", matrix->data[i][j]);
+            sprintf(tmp, "%c ", matrix->data[i][j] ?  ch1 : ch0);
             strcat(ans, tmp);
         }
         strcat(ans, "\n");
     }
     free(tmp);
     return ans;
+}
+
+char *gf2_matrix_dump(const struct GF2_Matrix *matrix) {
+    return gf2_matrix_dump_core(matrix, '0', '1');
 }
 
 void gf2_matrix_save(const struct GF2_Matrix *matrix, const char *file_path) {
@@ -125,4 +195,24 @@ struct GF2_Matrix *gf2_matrix_load(const char *file_path) {
     fp = NULL;
     return matrix;
 }
+
+bool gf2_matrix_is_identity(const struct GF2_Matrix *matrix) {
+    if (matrix->row <= 0 || matrix->row != matrix->col) {
+        return false;
+    }
+
+    for (size_t i = 0; i < matrix->row; ++i) {
+        for (size_t j = 0; j < matrix->col; ++j) {
+            if (i == j && matrix->data[i][j] != 1) {
+                return false;
+            }
+            if (i != j && matrix->data[i][j] != 0) {
+                return false;
+            }
+        }
+    }
+
+    return true;
+}
+
 #endif //ISMDEEP_C_UTILS_GF2_MATRIX_H

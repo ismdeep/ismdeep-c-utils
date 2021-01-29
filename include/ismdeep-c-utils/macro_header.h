e6f234646304a6fb94cc22316aff5a74e1327225
L. Jiang <l.jiang.1024@gmail.com>
2021-01-29 21:25:20 +0800

    update macro_header.h

--- include/ismdeep-c-utils/macro_header.h
+++ include/ismdeep-c-utils/macro_header.h
@@ -8,9 +8,9 @@
 #include <stdint.h>
 
 #define LOOP(type, id, size) for(type id = 0; id < (size); ++id)
-#define TIMES_zu(id, size) LOOP(size_t, id, size)
-#define TIMES_d(id, size) LOOP(int, id, size)
-#define TIMES_ull(id, size) LOOP(uint64_t, id, size)
+#define LOOP_zu(id, size) LOOP(size_t, id, size)
+#define LOOP_d(id, size) LOOP(int, id, size)
+#define LOOP_ull(id, size) LOOP(uint64_t, id, size)
 
 
 #define MIN(x, y) (((x) < (y) ? (x) : (y)))
